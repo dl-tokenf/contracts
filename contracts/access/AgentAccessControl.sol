97a2d11027e06349903e89427c007e04090655c8
dovgopoly <idovgopoly@gmail.com>
2024-03-04 14:26:04 +0200

    fix

--- contracts/access/AgentAccessControl.sol
+++ contracts/access/AgentAccessControl.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import {DiamondAccessControl} from "@solarity/solidity-lib/diamond/access/DiamondAccessControl.sol";
+import {DiamondAccessControl} from "@solarity/solidity-lib/diamond/access/access-control/DiamondAccessControl.sol";
 
 import {IAgentAccessControl} from "../interfaces/IAgentAccessControl.sol";
 
