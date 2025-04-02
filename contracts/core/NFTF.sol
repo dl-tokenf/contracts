af06f9a2716a46aa340430b7ff55e936441a7528
aritkulova <94910987+aritkulova@users.noreply.github.com>
2025-04-02 19:17:18 +0300

    added NFT F initial version

---
+++ contracts/core/NFTF.sol
@@ -0,0 +1,245 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import {IERC721} from "@openzeppelin/contracts/interfaces/IERC721.sol";
+import {ERC721EnumerableUpgradeable, ERC721Upgradeable, IERC165} from "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721EnumerableUpgradeable.sol";
+
+import {Diamond} from "@solarity/solidity-lib/diamond/Diamond.sol";
+
+import {INFTF} from "../interfaces/INFTF.sol";
+import {IKYCCompliance} from "../interfaces/IKYCCompliance.sol";
+import {IRegulatoryCompliance} from "../interfaces/IRegulatoryCompliance.sol";
+
+// solhint-disable-next-line no-global-import
+import "./Globals.sol";
+
+import {AgentAccessControl, AccessControlUpgradeable} from "./AgentAccessControl.sol";
+import {NFTFStorage} from "./storages/NFTFStorage.sol";
+import {RegulatoryComplianceStorage} from "./storages/RegulatoryComplianceStorage.sol";
+import {KYCComplianceStorage} from "./storages/KYCComplianceStorage.sol";
+
+abstract contract NFTF is
+    INFTF,
+    NFTFStorage,
+    Diamond,
+    ERC721EnumerableUpgradeable,
+    AgentAccessControl
+{
+    bytes4 public constant TRANSFER_SELECTOR = this.transfer.selector;
+    bytes4 public constant TRANSFER_FROM_SELECTOR = this.transferFrom.selector;
+    bytes4 public constant MINT_SELECTOR = this.mint.selector;
+    bytes4 public constant BURN_SELECTOR = this.burn.selector;
+    bytes4 public constant FORCED_TRANSFER_SELECTOR = this.forcedTransfer.selector;
+    bytes4 public constant RECOVERY_SELECTOR = this.recovery.selector;
+
+    function __NFTF_init(
+        address regulatoryCompliance_,
+        address kycCompliance_,
+        bytes memory initRegulatory_,
+        bytes memory initKYC_
+    ) internal virtual onlyInitializing {
+        bytes4[] memory rComplianceSelectors_ = new bytes4[](6);
+        rComplianceSelectors_[0] = IRegulatoryCompliance.addRegulatoryModules.selector;
+        rComplianceSelectors_[1] = IRegulatoryCompliance.removeRegulatoryModules.selector;
+        rComplianceSelectors_[2] = IRegulatoryCompliance.transferred.selector;
+        rComplianceSelectors_[3] = IRegulatoryCompliance.canTransfer.selector;
+        rComplianceSelectors_[4] = RegulatoryComplianceStorage.getRegulatoryModules.selector;
+        rComplianceSelectors_[5] = RegulatoryComplianceStorage.getRegulatoryModulesCount.selector;
+
+        bytes4[] memory kycComplianceSelectors_ = new bytes4[](5);
+        kycComplianceSelectors_[0] = IKYCCompliance.addKYCModules.selector;
+        kycComplianceSelectors_[1] = IKYCCompliance.removeKYCModules.selector;
+        kycComplianceSelectors_[2] = IKYCCompliance.isKYCed.selector;
+        kycComplianceSelectors_[3] = KYCComplianceStorage.getKYCModules.selector;
+        kycComplianceSelectors_[4] = KYCComplianceStorage.getKYCModulesCount.selector;
+
+        Facet[] memory facets_ = new Facet[](2);
+        facets_[0] = Facet(regulatoryCompliance_, FacetAction.Add, rComplianceSelectors_);
+        facets_[1] = Facet(kycCompliance_, FacetAction.Add, kycComplianceSelectors_);
+
+        _diamondCut(facets_, address(0), "");
+        _diamondCut(new Facet[](0), regulatoryCompliance_, initRegulatory_);
+        _diamondCut(new Facet[](0), kycCompliance_, initKYC_);
+    }
+
+    /// @inheritdoc INFTF
+    function transfer(address to_, uint256 tokenId_) public virtual override {
+        _canTransfer(msg.sender, to_, tokenId_, address(0));
+        _isKYCed(msg.sender, to_, tokenId_, address(0));
+
+        super._transfer(msg.sender, to_, tokenId_);
+
+        _transferred(msg.sender, to_, tokenId_, address(0));
+    }
+
+    /// @inheritdoc IERC721
+    function transferFrom(
+        address from_,
+        address to_,
+        uint256 tokenId_
+    ) public virtual override(ERC721Upgradeable, IERC721) {
+        _canTransfer(from_, to_, tokenId_, msg.sender);
+        _isKYCed(from_, to_, tokenId_, msg.sender);
+
+        super.transferFrom(from_, to_, tokenId_);
+
+        _transferred(from_, to_, tokenId_, msg.sender);
+    }
+
+    /// @inheritdoc INFTF
+    function mint(
+        address account_,
+        uint256 tokenId_
+    ) public virtual override onlyRole(_mintRole()) {
+        _canTransfer(address(0), account_, tokenId_, msg.sender);
+        _isKYCed(address(0), account_, tokenId_, msg.sender);
+
+        super._mint(account_, tokenId_);
+
+        _transferred(address(0), account_, tokenId_, msg.sender);
+    }
+
+    /// @inheritdoc INFTF
+    function burn(uint256 tokenId_) public virtual override onlyRole(_burnRole()) {
+        address account_ = _ownerOf(tokenId_);
+
+        _canTransfer(account_, address(0), tokenId_, msg.sender);
+        _isKYCed(account_, address(0), tokenId_, msg.sender);
+
+        super._burn(tokenId_);
+
+        _transferred(account_, address(0), tokenId_, msg.sender);
+    }
+
+    /// @inheritdoc INFTF
+    function forcedTransfer(
+        address from_,
+        address to_,
+        uint256 tokenId_
+    ) public virtual override onlyRole(_forcedTransferRole()) {
+        _canTransfer(from_, to_, tokenId_, msg.sender);
+        _isKYCed(from_, to_, tokenId_, msg.sender);
+
+        super._transfer(from_, to_, tokenId_);
+
+        _transferred(from_, to_, tokenId_, msg.sender);
+    }
+
+    /// @inheritdoc INFTF
+    function recovery(
+        address oldAccount_,
+        address newAccount_
+    ) public virtual override onlyRole(_recoveryRole()) {
+        uint256 oldBalance_ = balanceOf(oldAccount_);
+
+        for (uint256 i = oldBalance_; i > 0; --i) {
+            uint256 tokenId_ = tokenOfOwnerByIndex(oldAccount_, i - 1);
+
+            _canTransfer(oldAccount_, newAccount_, tokenId_, msg.sender);
+            _isKYCed(oldAccount_, newAccount_, tokenId_, msg.sender);
+
+            super._transfer(oldAccount_, newAccount_, tokenId_);
+
+            _transferred(oldAccount_, newAccount_, tokenId_, msg.sender);
+        }
+    }
+
+    /// @inheritdoc INFTF
+    function diamondCut(
+        Facet[] memory modules_
+    ) public virtual override onlyRole(_diamondCutRole()) {
+        diamondCut(modules_, address(0), "");
+    }
+
+    /// @inheritdoc INFTF
+    function diamondCut(
+        Facet[] memory modules_,
+        address initModule_,
+        bytes memory initData_
+    ) public virtual override onlyRole(_diamondCutRole()) {
+        _diamondCut(modules_, initModule_, initData_);
+    }
+
+    /// @inheritdoc IERC165
+    function supportsInterface(
+        bytes4 interfaceId_
+    )
+        public
+        view
+        override(AccessControlUpgradeable, ERC721EnumerableUpgradeable, IERC165)
+        returns (bool)
+    {
+        return
+            ERC721EnumerableUpgradeable.supportsInterface(interfaceId_) ||
+            AccessControlUpgradeable.supportsInterface(interfaceId_);
+    }
+
+    function _transferred(
+        address from_,
+        address to_,
+        uint256 tokenId_,
+        address operator_
+    ) internal virtual {
+        try
+            IRegulatoryCompliance(address(this)).transferred(
+                Context(bytes4(bytes(msg.data[:4])), from_, to_, 0, tokenId_, operator_, "")
+            )
+        {} catch {
+            revert TransferredReverted();
+        }
+    }
+
+    function _canTransfer(
+        address from_,
+        address to_,
+        uint256 tokenId_,
+        address operator_
+    ) internal view virtual {
+        try
+            IRegulatoryCompliance(address(this)).canTransfer(
+                Context(bytes4(bytes(msg.data[:4])), from_, to_, 0, tokenId_, operator_, "")
+            )
+        returns (bool canTransfer_) {
+            require(canTransfer_, CannotTransfer());
+        } catch {
+            revert CanTransferReverted();
+        }
+    }
+
+    function _isKYCed(
+        address from_,
+        address to_,
+        uint256 tokenId_,
+        address operator_
+    ) internal view virtual {
+        try
+            IKYCCompliance(address(this)).isKYCed(
+                Context(bytes4(bytes(msg.data[:4])), from_, to_, 0, tokenId_, operator_, "")
+            )
+        returns (bool isKYCed_) {
+            require(isKYCed_, NotKYCed());
+        } catch {
+            revert IsKYCedReverted();
+        }
+    }
+
+    function _mintRole() internal view virtual returns (bytes32) {
+        return AGENT_ROLE;
+    }
+
+    function _burnRole() internal view virtual returns (bytes32) {
+        return AGENT_ROLE;
+    }
+
+    function _forcedTransferRole() internal view virtual returns (bytes32) {
+        return AGENT_ROLE;
+    }
+
+    function _recoveryRole() internal view virtual returns (bytes32) {
+        return AGENT_ROLE;
+    }
+
+    function _diamondCutRole() internal view virtual returns (bytes32) {
+        return AGENT_ROLE;
+    }
+}
---
+++ contracts/interfaces/INFTF.sol
@@ -0,0 +1,150 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import {IERC721Metadata} from "@openzeppelin/contracts/token/ERC721/extensions/IERC721Metadata.sol";
+
+import {Diamond} from "@solarity/solidity-lib/diamond/Diamond.sol";
+
+/**
+ * @notice The `NFTF` contract
+ *
+ * The `NFTF` is a Diamond-based ERC721 token implementation enabling the storage of all core contracts under the same Diamond proxy.
+ *
+ * The `NFTF` provides flexibility for implementing eligibility checks through the integration of compliance modules without
+ * affecting the standard ERC721 behaviour.
+ *
+ * Transfer methods forward the entire transfer context to compliance modules, ensuring adherence to specific requirements,
+ * such as regulatory standards or KYC protocols.
+ *
+ * `NFTF` is also inherited from `AgentAccessControl`, which is built on OpenZeppelin's `AccessControlUpgradeable`.
+ * This inheritance allows to realise a rather flexible system of roles for controlling privileged functions in the whole system.
+ *
+ * Inheritance from `ERC721EnumerableUpgradeable` allows checking all tokens owned by a specific address.
+ */
+interface INFTF is IERC721Metadata {
+    /**
+     * @notice Function for transfering `NFTF` token from the message sender to another address.
+     *
+     * The `isKYCed` hook from the `KYCCompliance` contract is used inside the function to check KYC.
+     * The `canTransfer` and `transferred` hooks are used to check the established regulatory rules
+     * from `RegulatoryCompliance` contract.
+     *
+     *
+     * @param to_ The user address to whom token will be transferred
+     * @param tokenId_ The id of the token to be transferred
+     */
+    function transfer(address to_, uint256 tokenId_) external;
+
+    /**
+     * @notice Function to create new `NFTF` contract token.
+     *
+     * The `isKYCed` hook from the `KYCCompliance` contract is used inside the function to check KYC.
+     * The `canTransfer` and `transferred` hooks are used to check the established regulatory rules
+     * from `RegulatoryCompliance` contract
+     *
+     * This function can only be called by users who have a special role.
+     * In the base version of `NFTF` this role is the Agent role.
+     *
+     * To change the role used for access validation,
+     * you need to override `_mintRole` according to the requirements of your business task.
+     *
+     * @param account_ The address to which tokens should be minted
+     * @param tokenId_ The id of the token to be minted
+     */
+    function mint(address account_, uint256 tokenId_) external;
+
+    /**
+     * @notice Function to burn existing `NFTF` contract token.
+     *
+     * The `isKYCed` hook from the `KYCCompliance` contract is used within the function to check KYC.
+     * The `canTransfer` and `transferred` hooks are used to check the established regulatory rules
+     * from the `RegulatoryCompliance` contract.
+     *
+     * This function can only be called by users who have a special role.
+     * In the base version of `NFTF` this role is the Agent role.
+     *
+     * To change the role used for access verification,
+     * you must override `_burnRole` according to the requirements of your business task.
+     *
+     * @param tokenId_ The id of the token to be burned
+     */
+    function burn(uint256 tokenId_) external;
+
+    /**
+     * @notice Function for forced transfering from one address to another.
+     *
+     * This logic may be needed for smart contracts that will operate with `NFTF` tokens.
+     * It will be convenient because users will not need to call `approve` function additionally.
+     *
+     * The `isKYCed` hook from the `KYCCompliance` contract is used inside the function to check KYC.
+     * The `canTransfer` and `transferred` hooks are used to check the established regulatory rules
+     * from `RegulatoryCompliance` contract.
+     *
+     * This function can only be called by users who have a special role.
+     * In the base version of `NFTF` this role is the Agent role.
+     *
+     * To change the role used for access verification,
+     * you must override `_forcedTransferRole` according to the requirements of your business task.
+     *
+     * @param from_ The user address where the token will be transferred from
+     * @param to_ The user address to whom token will be transferred
+     * @param tokenId_ The id of the token to be transferred
+     */
+    function forcedTransfer(address from_, address to_, uint256 tokenId_) external;
+
+    /**
+     * @notice Function for balance transfer from one account to another.
+     *
+     * This function can be useful if the user has lost the private key to his account.
+     * In this case, the system can check the user's KYC and transfer the user's tokens to a new account.
+     *
+     * The `isKYCed` hook from the `KYCCompliance` contract is used inside the function to check the KYC.
+     * The `canTransfer` and `transferred` hooks are used to check the established regulatory rules
+     * from `RegulatoryCompliance` contract.
+     *
+     * This function can only be called by users who have a special role.
+     * In the base version of `NFTF` this role is the Agent role.
+     *
+     * To change the role used for access verification,
+     * you need to redefine `_recoveryRole` according to the requirements of your business task.
+     *
+     * @param oldAccount_ The address of the user's old account
+     * @param newAccount_ The address of the new user account to which the tokens will be migrated
+     */
+    function recovery(address oldAccount_, address newAccount_) external;
+
+    /**
+     * @notice The function is required to manage the list of facets in a `NFTF` contract.
+     * It can be used to add, delete and update existing facets.
+     *
+     * This function can only be called by users who have a special role.
+     * In the basic version of `NFTF` this role is the Agent role.
+     *
+     * To change the role used for access validation,
+     * you must override `_diamondCutRole` according to the requirements of your business task
+     *
+     * @param modules_ The array of modules to update
+     */
+    function diamondCut(Diamond.Facet[] memory modules_) external;
+
+    /**
+     * @notice This function overloads another `diamondCut` function `NFTF` of the contract,
+     * allowing you to pass an additional calldata to call the necessary methods using `delegateCall` on the facet address.
+     * Often this calldata is needed to call various init functions.
+     *
+     * This function can only be called by users who have a special role.
+     * In the basic version of `NFTF` this role is the Agent role.
+     *
+     * To change the role used for access validation,
+     * you must override `_diamondCutRole` according to the requirements of your business task.
+     *
+     * @param modules_ The array of modules to update
+     * @param initModule_ The address of the module to execute the passed caldata
+     * @param initData_ The calldata to be executed
+     */
+    function diamondCut(
+        Diamond.Facet[] memory modules_,
+        address initModule_,
+        bytes memory initData_
+    ) external;
+}
