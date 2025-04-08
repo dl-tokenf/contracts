29e3462fc8eabe00acab70f6ab8735887154d695
aritkulova <94910987+aritkulova@users.noreply.github.com>
2025-04-08 22:24:06 +0300

    added onlyInitializing to init functions

--- examples/equity-token/contracts/EquityKYCCompliance.sol
+++ examples/equity-token/contracts/EquityKYCCompliance.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.20;
 import {KYCCompliance} from "@tokenf/contracts/core/KYCCompliance.sol";
 
 contract EquityKYCCompliance is KYCCompliance {
-    function __EquityKYCCompliance_init() external {
+    function __EquityKYCCompliance_init() external onlyInitializing {
         __KYCCompliance_init();
     }
 }
--- examples/equity-token/contracts/EquityRegulatoryCompliance.sol
+++ examples/equity-token/contracts/EquityRegulatoryCompliance.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.20;
 import {RegulatoryCompliance} from "@tokenf/contracts/core/RegulatoryCompliance.sol";
 
 contract EquityRegulatoryCompliance is RegulatoryCompliance {
-    function __EquityRegulatoryCompliance_init() external {
+    function __EquityRegulatoryCompliance_init() external onlyInitializing {
         __RegulatoryCompliance_init();
     }
 }
